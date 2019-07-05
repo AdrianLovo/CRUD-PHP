d317b947c9d51ddca948b8877e689b167e78366b
Adrian Castillo <adrianhumbertocastillo@gmail.com>
2019-07-05 13:55:00 -0500

    Agregada funcion js para vistaPrevia de imagen de cada registro en DataTable

--- VistaController/ControladorPersona.js
+++ VistaController/ControladorPersona.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	Listar();
+	screenshotPreview();
 });
 
 //Funciones principales Listar, Agregar, Eliminar y Modificar
@@ -163,4 +164,32 @@ function ValidarTipoArchivo(){
         }
     });
 }
+
+
+
+ 
+function screenshotPreview(){	
+	xOffset = 10;
+	yOffset = 30;
+	
+	$("a.screenshot").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<p id='screenshot'><img src='"+ this.rel +"' alt='url preview' />"+ c +"</p>");								 
+		$("#screenshot")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+	function(){
+		this.title = this.t;	
+		$("#screenshot").remove();
+    });	
+	$("a.screenshot").mousemove(function(e){
+		$("#screenshot")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
 
